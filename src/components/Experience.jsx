2ad3b6406c58ba4faf6cac28deaa79da8fffe8bb
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-04-17 06:38:48 +0530

    add environment, sky, shadows, and base mesh to Experience scene

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -6,22 +6,44 @@ export const Experience = () => {
 
     const { animation } = useControls({
         animation: {
-
             value: "Typing",
-            options: ["Typing", "Falling", "Standing"]
-        }
-    })
+            options: ["Typing", "Falling", "Standing"],
+        },
+    });
 
     return (
         <>
             <OrbitControls />
+            <Sky />
+            <Environment preset="sunset" />
             <group position-y={-1} >
+                <ContactShadows
+                    opacity={0.42}
+                    scale={10}
+                    blur={1}
+                    far={10}
+                    resolution={256}
+                    color="#000000"
+                />
                 <Avatar
                     animation={animation}
                 />
+                {animation === "Typing" && (
+                    <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25} >
+                        <boxGeometry />
+                        <meshStandardMaterial color="white" />
+                    </mesh>
+                )}
 
+                <mesh
+                    scale={5}
+                    rotation-x={-Math.PI * 0.5}
+                    position-y={-0.001}
+                >
+                    <planeGeometry />
+                    <meshStandardMaterial color="white" />
+                </mesh>
             </group>
-            <ambientLight intensity={1} />
         </>
     );
 };
