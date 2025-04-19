c112d5a756c37dcb849cd2e74c1d2bf684daa6a8
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-04-19 22:45:05 +0530

    add office model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
+import { Office } from "./Office";
 
 export const Experience = () => {
 
@@ -43,6 +44,8 @@ export const Experience = () => {
                     <planeGeometry />
                     <meshStandardMaterial color="white" />
                 </mesh>
+
+                {/* <Office /> */}
             </group>
         </>
     );
